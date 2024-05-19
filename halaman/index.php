1c417b41230b6cf1027fd43ffdc7e1be9b3c4be4
ahmaddzakis <ahmaddzaki141@gmail.com>
2024-05-19 11:40:38 +0700

    iloveu

--- halaman/index.php
+++ halaman/index.php
@@ -17,7 +17,71 @@ if(!isset($_SESSION["login"])) {
   <link rel="stylesheet" href="../css/index.css">
 </head>
 <body>
-  <!-- <h1>Halo, <?= $_SESSION["email"]; ?></h1> -->
+  <!-- <h1>Halo, <?= $_SESSION["username"]; ?></h1> -->
   <a href="login.php">Logout</a>
 </body>
 </html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LOGIN</title>
+  <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row-left">
+      <h1>Halo, Selamat Datang Kembali</h1>
+      <h3>Masukkan alamat email Anda</h3>
+      <form action="" method="post">
+       <label>
+        Email
+        <input type="email" name="email" id="email" placeholder="Enter Email" required>
+       </label>
+       <label>
+        Password
+        <input type="password" name="password" id="password" placeholder="Enter Password" required>
+       </label>
+       <button type="submit" name="sign-in">Sign In</button>
+      </form>
+
+  
+    </div>
+    <div class="row-right">
+    <h1>Selamat Datang</h1>
+      <h3>Buat Akun</h3>
+      <form action="" method="post">
+       <label>
+        Email
+        <input type="email" name="email" id="email" placeholder="Enter Email" required>
+       </label>
+       <label>
+        Username
+        <input type="text" name="username" id="username" placeholder="Enter Username" required>
+       </label>
+       <label>
+        Password
+        <input type="password" name="password1" id="password" placeholder="Enter Password" required>
+       </label>
+       <label>
+        Konfirmasi Password
+        <input type="password" name="password2" id="password" placeholder="Enter Password Again" required>
+       </label>
+       <button type="submit" name="sign-up">Sign Up</button>
+      </form>
+
+    </div>
+    <?php if (isset($error)) : ?>
+      <div class="error">
+    <h1>Email / Password salah!</h1>
+    <p>Silakan coba lagi!</p>
+    <form action="">
+      <button type="submit">Lanjutkan</button>
+    </form>
+  </div>
+  <?php endif; ?>
+  </div>
+</body>
+</html> -->
--- halaman/login.php
+++ halaman/login.php
@@ -37,57 +37,65 @@ if(isset($_POST["sign-up"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LOGIN</title>
-  <link rel="stylesheet" href="../css/login.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Halaman Login</title>
 </head>
+
 <body>
-  <div class="container">
-    <div class="row-left">
-      <h1>Halo, Selamat Datang Kembali</h1>
-      <h3>Masukkan alamat email Anda</h3>
-      <form action="" method="post">
-       <label>
-        Email
-        <input type="email" name="email" id="email" placeholder="Enter Email" required>
-       </label>
-       <label>
-        Password
-        <input type="password" name="password" id="password" placeholder="Enter Password" required>
-       </label>
-       <button type="submit" name="sign-in">Sign In</button>
-      </form>
-
-  
-    </div>
-    <div class="row-right">
-    <h1>Selamat Datang</h1>
-      <!-- <h2>Sign Up</h2> -->
-      <h3>Buat Akun</h3>
-      <form action="" method="post">
-       <label>
-        Email
-        <input type="email" name="email" id="email" placeholder="Enter Email" required>
-       </label>
-       <label>
-        Username
-        <input type="text" name="username" id="username" placeholder="Enter Username" required>
-       </label>
-       <label>
-        Password
-        <input type="password" name="password1" id="password" placeholder="Enter Password" required>
-       </label>
-       <label>
-        Konfirmasi Password
-        <input type="password" name="password2" id="password" placeholder="Enter Password Again" required>
-       </label>
-       <button type="submit" name="sign-up">Sign Up</button>
-      </form>
-
-    </div>
-    <?php if (isset($error)) : ?>
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form action="" method="post">
+                <h1>Buat Akun</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>atau gunakan email anda untuk pendaftaran</span>
+                <input type="text" name="username" id="username" placeholder="Username" required>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <input type="password" name="password1" id="password" placeholder="Password" required>
+                <input type="password" name="password2" id="password" placeholder="Konfirmasi Password" required>
+                <button type="submit" name="sign-up">Mendaftar</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form action="" method="post">
+                <h1>Masuk</h1>
+                <div class="social-icons">
+                    <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>atau gunakan email password anda</span>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <a href="#">Lupa password?</a>
+                <button type="submit" name="sign-in">Masuk</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Selamat Datang kembali!</h1>
+                    <p>Masukkan detail pribadi Anda untuk menggunakan semua fitur situs</p>
+                    <button class="hidden" id="login">Masuk</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Halo, teman!</h1>
+                    <p>Daftarkan dengan detail pribadi Anda untuk menggunakan semua fitur situs</p>
+                    <button class="hidden" id="register">Mendaftar</button>
+                </div>
+            </div>
+        </div>
+        <!-- <?php if (isset($error)) : ?>
       <div class="error">
     <h1>Email / Password salah!</h1>
     <p>Silakan coba lagi!</p>
@@ -95,7 +103,9 @@ if(isset($_POST["sign-up"])) {
       <button type="submit">Lanjutkan</button>
     </form>
   </div>
-  <?php endif; ?>
-  </div>
+  <?php endif; ?> -->
+    </div>
+    <script src="../js/script.js"></script>
 </body>
+
 </html>
